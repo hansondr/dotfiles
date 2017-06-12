b2c6443b1c2a620daa9e38ba19dc33310d31f901
Daniel Hanson <hansondr@gmail.com>
2017-06-12 10:49:05 -0600

    Remove window title bars, tweak Spotify notifications

--- polybar/.config/polybar/spotify.sh
+++ polybar/.config/polybar/spotify.sh
@@ -14,7 +14,7 @@ main() {
 
   artist=$(echo "$meta" | sed -nr '/xesam:artist"/,+2s/^ +string "(.*)"$/\1/p' | tail -1 | sed "s/\&/+/g")
   album=$(echo "$meta" | sed -nr '/xesam:album"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1)
-  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed "s/\&/+/g" )
+  title=$(echo "$meta" | sed -nr '/xesam:title"/,+2s/^ +variant +string "(.*)"$/\1/p' | tail -1 | sed "s/[\&\/]/+/g" )
 
   echo "${*:-%artist% - %title%}" | sed "s/%artist%/$artist/g;s/%title%/$title/g;s/%album%/$album/g"i
 }
